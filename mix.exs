eb93f54fbee1d2a0476e0f9a5ee917fbc9320493
Lexin Gong <gonglexin@gmail.com>
2025-09-19 11:06:42 +0800

    :arrow_up: Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Mahjong.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:tidewave, "~> 0.4", only: :dev},
+      {:tidewave, "~> 0.5", only: :dev},
       {:live_debugger, "~> 0.4.0", only: :dev}
     ]
   end
